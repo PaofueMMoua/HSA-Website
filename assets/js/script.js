4ea092a2cbb0b7205554a0c7948ce33cb89b6af5
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2025-07-03 09:47:08 -0700

    fixing things

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,29 +27,17 @@ function showSlides(n) {
 
 function menu() {
   let x = document.getElementById("dropContent");
-  if (x.style.display = 'flex') {
-    x.display = "none";
-  } else {
-    x.display = "flex";
-  } 
+  x.classList.toggle("showFlex")
 }
 
-let dropContent = document.getElementById('dropContent')
+
 function show() {
+  let dropContent = document.getElementById('dropContent')
   dropContent.classList.toggle("show")
 }
 
-// let famline = document.getElementById
 function famDrop() {
   let famline = document.getElementById('familything')
   let temp = famline.classList
   temp.toggle('show')
 }
-
-
-// let meeting_Social = document.getElementById('weeklyMeetingAndMonthlySocialButton');
-
-// metting_social.addEventListener('click', () => {
-//   let x = document.getElementById('Meeting&SocialText');
-//   if (x.display = )
-// })
